89eea018d0dd109bd4af21bae6dc1ad4bff0e700
stickblue <1251670584@qq.com>
2020-03-23 08:34:13 +0000

    Add fragment.

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.coolweather;
+package com.coolweather.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
